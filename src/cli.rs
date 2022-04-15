196e9cd0bfd6f837d2d36fa99fc15fb6597ba25c
Stevie Alvarez <stevie.6strings@gmail.com>
2022-04-14 21:27:36 -0400

    implemented command_selector, help, and hello_world

--- src/cli.rs
+++ src/cli.rs
@@ -3,3 +3,34 @@
 pub fn run() {
   println!("cli run called");
 }
+
+/// Run the specified command
+/// 
+/// if command exists, run and return true
+/// 
+/// if command does not exist, return false
+pub fn command_selector(c: String) -> bool {
+  if c == "help" {
+    help();
+
+  } else if c == "hello" {
+    hello_world();
+
+  } else {
+    println!("Usage: rust-cli [OPTIONS...]\nTry: 'rust-cli --help' for more information.");
+    return false;
+  }
+
+  true
+}
+
+/// Print CLI help
+fn help() {
+  println!("Usage: rust-cli [OPTIONS...]\n\n--help\n\tprint out program help information
+  \n\n--hello\n\tprint \"Hello World!\"");
+}
+
+/// Print "Hello World!"
+fn hello_world() {
+  println!("Hello World!");
+}
