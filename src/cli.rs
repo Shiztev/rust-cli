508a73af0b10ce7182c655ee5b7f0c53f6c906ec
Stevie Alvarez <stevie.6strings@gmail.com>
2022-04-15 11:07:40 -0400

    clear input buffer, command_selector takes &str, print new lineat end of command_selector

--- src/cli.rs
+++ src/cli.rs
@@ -1,39 +1,42 @@
 /// Command line prompt
 
-use std::io::{stdin, stdout, Write};
+use std::{io::{stdin, stdout, Write}, process::exit};
 
 pub fn run() {
   let mut buf: String = String::new();
 
-  // prompt user, handle output errors
-  print!("r| ");
-  stdout().flush().expect("Could not flush out command prompt");
+  loop {
+    // prompt user, handle output errors
+    print!("r| ");
+    stdout().flush().expect("Could not flush out command prompt");
 
-  // get input, handle errors
-  stdin().read_line(&mut buf).expect("failed to read line");
+    // get input, handle errors
+    stdin().read_line(&mut buf).expect("failed to read line");
 
-  // run respective command
-  command_selector(&buf);
-
-
-
-  println!("you entered: {}", buf);
+    // run respective command
+    command_selector(buf.trim());
+    buf.clear();
+  }
 }
 
 /// Run the specified command
-pub fn command_selector(c: &String) {
+pub fn command_selector(c: &str) {
 
-  match c.trim() {
+  match c {
+    "quit"  => exit(0),
     "help" => help(),
     "hello" => hello_world(),
-    _ => println!("Usage: rust-cli [OPTIONS...]\nTry: 'rust-cli --help' for more information.")
+    _ => println!("rust-cli: no command '{}'\nTry: 'rust-cli --help' for more information.", c)
   }
+
+  println!();
 }
 
 /// Print CLI help
 fn help() {
-  println!("Usage: rust-cli [OPTIONS...]\n\nhelp\n\tprint out program help information
-  \n\nhello\n\tprint \"Hello World!\"");
+  println!("Usage: rust-cli [OPTIONS...]
+  \n\n\thelp\n\t\tprint out program help information
+  \n\n\thello\n\t\tprint \"Hello World!\"");
 }
 
 /// Print "Hello World!"
