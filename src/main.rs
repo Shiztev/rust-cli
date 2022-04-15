81b7f4c540d8b89acb17d677a982968da905c29c
Stevie Alvarez <stevie.6strings@gmail.com>
2022-04-14 20:49:27 -0400

    added file documentation, use std env, get args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+/// Small CLI application.
+/// 
+/// Author: Stevie Alvarez
+
+use std::env;
+
 fn main() {
   // handle command line args
+  let args: Vec<String> = env::args().collect();
 
   // loop on user input
 }
